7e23611c5fc9386c5a855eceea6a1b59afd9b020
lanford33 <lanfordcai@outlook.com>
2022-12-12 20:24:48 +0800

    feat: click collection view to jump

--- components/common/CollectionView.js
+++ components/common/CollectionView.js
@@ -2,16 +2,24 @@ import Image from "next/image"
 import publicConfig from "../../publicConfig"
 import { getImageSrcFromMetadataViewsFile } from "../../lib/utils"
 import { useRouter } from "next/router"
+import { ArrowRightIcon } from "@heroicons/react/outline"
 
 export default function CollectionView(props) {
   const router = useRouter()
   const { account, collection } = props
-  if (collection.tokenIDs.length > 0) {
-    console.log(collection)
-  }
 
   return (
-    <div className="flex flex-col max-w-[1094px] min-w-[1076px] gap-y-3 p-4 shadow-md rounded-2xl bg-white">
+    <div className="flex flex-col max-w-[1094px] min-w-[1076px] gap-y-3 p-4 shadow-md rounded-2xl bg-white"
+      onClick={() => {
+        if (collection.tokenIDs.length == 0) {
+          return
+        }
+        router.push({
+          pathname: `${router.pathname}/[collection]`,
+          query: { account: account, collection: collection.path.replace("/storage/", "") }
+        }, undefined, { shallow: true })
+      }}
+    >
       <div className="flex gap-x-3 justify-between">
         <div className="flex items-center gap-x-3">
           <div className="w-11 rounded-full overflow-hidden aspect-square relative">
@@ -34,30 +42,24 @@ export default function CollectionView(props) {
           {
             collection.collectionIdentifier ?
               <label className={`cursor-pointer shrink-0`}>
-                <a href={`${publicConfig.nftCatalogURL}/${collection.collectionIdentifier}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-
-                  <div className="h-[24px] aspect-square shrink-0 relative">
-                    <Image src={"/nft-catalog.png"} alt="" fill sizes="10vw" className="object-contain" />
-                  </div>
-                </a>
+                <div className="h-[24px] aspect-square shrink-0 relative"
+                  onClick={(event) => {
+                    window.open(`${publicConfig.nftCatalogURL}/${collection.collectionIdentifier}`)
+                    event.stopPropagation()
+                  }}>
+                  <Image src={"/nft-catalog.png"} alt="" fill sizes="10vw" className="object-contain" />
+                </div>
               </label> : null
           }
           {
             <button
               type="button"
-              className={"text-black bg-drizzle disabled:bg-drizzle-light disabled:text-gray-400 hover:bg-drizzle-dark px-3 py-2 text-sm rounded-2xl font-semibold shrink-0"}
+              className={"text-drizzle disabled:text-drizzle-light shrink-0"}
               disabled={collection.tokenIDs.length == 0}
               onClick={async () => {
-                router.push({
-                  pathname: `${router.pathname}/[collection]`,
-                  query: { account: account, collection: collection.path.replace("/storage/", "") }
-                }, undefined, { shallow: true })
               }}
             >
-              {`Show Detail`}
+              <ArrowRightIcon className=" w-[24px] aspect-square shrink-0" />
             </button>
           }
         </div>
