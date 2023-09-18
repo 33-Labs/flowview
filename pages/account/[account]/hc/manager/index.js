5e74aa30e1194d04b94e862c6e7c8c6d0a1b2e3b
lanford33 <lanfordcai@outlook.com>
2023-09-18 08:05:15 +0800

    fix: layout

--- pages/account/[account]/hc/manager/index.js
+++ pages/account/[account]/hc/manager/index.js
@@ -110,7 +110,7 @@ export default function HybridCustodyManager(props) {
   return (
     <div className="container mx-auto max-w-7xl min-w-[380px] px-2">
       <Layout>
-        <div className="flex w-full flex-col gap-y-3 overflow-auto">
+        <div className="min-w-[1076px] flex w-full flex-col gap-y-3 overflow-auto">
           <div className="p-2 flex gap-x-2 justify-between w-full">
             <h1 className="text-xl sm:text-2xl font-bold text-gray-900">
               {`HybridCustody Manager`}
--- pages/account/[account]/hc/owned_account/index.js
+++ pages/account/[account]/hc/owned_account/index.js
@@ -83,7 +83,7 @@ export default function HybridCustodyOwnedAcct(props) {
   return (
     <div className="container mx-auto max-w-7xl min-w-[380px] px-2">
       <Layout>
-        <div className="flex w-full flex-col gap-y-3 overflow-auto">
+        <div className="min-w-[1076px] flex w-full flex-col gap-y-3 overflow-auto">
           <div className="p-2 flex gap-x-2 justify-between w-full">
             <div className="flex flex-col gap-y-1">
               <h1 className="text-xl sm:text-2xl font-bold text-gray-900">
